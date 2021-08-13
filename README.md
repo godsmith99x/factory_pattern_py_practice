85e25f4934665e1dd161dc718505ae9be19cecd6
Joel <joel@pop-os.localdomain>
2021-08-12 22:11:40 -0400

    Added py file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # factory_pattern_py_practice
+
+
+Practicing the factory pattern following ArjanCodes' video:
+https://youtu.be/s_4ZrtQs8Do
---
+++ factory_pattern.py
@@ -0,0 +1,6 @@
+""""
+Example of factory class using inheritance from an abstract class 
+""""
+
+import pathlib
+from abc import ABC, abstractmethod
